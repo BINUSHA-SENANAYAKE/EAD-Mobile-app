5318bc93114629aca75d010312e3dbbd65608d1f
Binusha <it20612696@my.sliit.lk>
2023-10-01 19:52:05 +0530

    Available Trains UI completed

--- app/src/main/java/com/example/sliit_travel_app/HomePage.java
+++ app/src/main/java/com/example/sliit_travel_app/HomePage.java
@@ -2,7 +2,13 @@ package com.example.sliit_travel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sliit_travel_app.databinding.ActivityLoginBinding;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Button button_d1 = findViewById(R.id.doc_page1_btn);
+        Button button_d2 = findViewById(R.id.doc_page1_btn2);
+
+        button_d1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, TrainSchedule.class);
+                startActivity(intent);
+            }
+        }
+        );
     }
 }
