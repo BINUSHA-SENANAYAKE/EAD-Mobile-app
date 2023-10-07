aa72e2da732829bf127d3fda0d7c7ba9ff784307
Binusha <it20612696@my.sliit.lk>
2023-10-07 23:52:48 +0530

    Reservation Amount view page UI completed

---
+++ app/src/main/java/com/example/sliit_travel_app/Reservation_ammount.java
@@ -0,0 +1,14 @@
+package com.example.sliit_travel_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reservation_ammount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_ammount);
+    }
+}
--- app/src/main/java/com/example/sliit_travel_app/selected_reservation_details.java
+++ app/src/main/java/com/example/sliit_travel_app/selected_reservation_details.java
@@ -2,7 +2,10 @@ package com.example.sliit_travel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class selected_reservation_details extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class selected_reservation_details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_reservation_details);
+
+        Button button_d1 = findViewById(R.id.doc_page4_btn1);
+
+        button_d1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(selected_reservation_details.this, Reservation_ammount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
